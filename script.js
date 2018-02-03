5b43c60347002072ab633376d2a380e6fc615409
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-03 01:12:05 +0100

    Type coercion PART II

--- script.js
+++ script.js
@@ -16,4 +16,16 @@ var name = 'John';
 var age = 26;
 
 console.log(name + age);
+console.log(age + age);
 
+var job, isMarried;
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' ' + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'Thirthy six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' ' + ' years old ' + job + '. Is he married? ' + isMarried + '.');
