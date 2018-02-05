3cccdfd272fd7e7f6e54fcf4cf43764c06c5aae5
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-05 12:05:18 +0100

    Basic IF ELSE statement

--- script.js
+++ script.js
@@ -37,29 +37,67 @@
 
 
 
-// Operators
+// /////////////////////////////////// Operators /////////////////////////////////////////
 
-var now= 2016;
+// var now= 2016;
 
-var birthYear = now - 26;
+// var birthYear = now - 26;
 
-birthYear = now - 26 * 2;
+// birthYear = now - 26 * 2;
 
-console.log(birthYear);
+// console.log(birthYear);
 
-var ageJohn = 30;
-var ageMark = 30;
+// var ageJohn = 30;
+// var ageMark = 30;
 
-ageJohn = ageMark = (3 + 5) * 4 - 6;
-// ageJohn = ageMark = 26
-// ageJohn = 26;
+// ageJohn = ageMark = (3 + 5) * 4 - 6;
+// // ageJohn = ageMark = 26
+// // ageJohn = 26;
 
-ageJohn++;
-// ageJohn = ageJohn + 1;
+// ageJohn++;
+// // ageJohn = ageJohn + 1;
 
-ageMark*= 2;
-// ageMark = ageMark * 2;
+// ageMark*= 2;
+// // ageMark = ageMark * 2;
 
 
-console.log(ageJohn);
-console.log(ageMark);
+// console.log(ageJohn);
+// console.log(ageMark);
+
+
+
+/////////////////////////////////// IF ELSE /////////////////////////////////////////
+
+var name = 'John';
+var age = 26;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married !');
+} else {
+    console.log(name + ' will hopefully marry soon !');
+}
+
+
+isMarried = true;
+
+// if(isMarried) {
+//     console.log('YES!');
+// } else {
+//     console.log('NO!');
+// }
+
+if(isMarried) {
+    console.log('YES!');
+}
+
+// Js does Type coercion
+if (23 == "23") {
+    console.log('Something to print...');
+}
+
+// Js doesnt do Type coercion, it sees that the 23 is as number ans "23" as string
+if (23 === "23") {
+    console.log('Something to print...');
+}
+
